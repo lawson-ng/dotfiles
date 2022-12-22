fd7a3041ca02b8028c611c167ec18e9bdcf116bf
lawson-ng <abraham.lawson.dev@gmail.com>
2022-12-23 01:17:57 +0700

    update: plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -16,10 +16,7 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   -- Theme -- 
   use "EdenEast/nightfox.nvim"  
-  use({
-	"nvim-treesitter/nvim-treesitter",
-	commit = "518e27589c0463af15463c9d675c65e464efc2fe",
-  })
+  use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use {
 	'nvim-lualine/lualine.nvim',
   	requires = { 'kyazdani42/nvim-web-devicons', opt = true }
