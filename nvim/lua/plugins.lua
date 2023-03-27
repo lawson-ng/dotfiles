52fea6550b195615984b4aebbb989bc90ea5ff02
lawson-ng <abraham.lawson.dev@gmail.com>
2023-03-27 22:13:47 +0700

    update

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -36,10 +36,6 @@ return require("packer").startup(function(use)
 	}
 	use"nvim-lua/plenary.nvim"
 	use{ "nvim-telescope/telescope-file-browser.nvim" }
-	use{
-		"dracula/vim",
-		name = "dracula",
-	}
 	use"jose-elias-alvarez/null-ls.nvim"
 	use"MunifTanjim/prettier.nvim"
 
